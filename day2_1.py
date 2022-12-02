4f0dcc937fcf76a9de700ddc25d991ce23a6459b
Sudhan Chitgopkar <sudhan@osx.local>
2022-12-02 01:08:13 -0500

    🔧 Edited Day 2

--- day2_1.py
+++ day2_1.py
@@ -1,23 +1,23 @@
 def solve (inputFile):
-    points = {
-        "A" : 1,
-        "B" : 2,
-        "C" : 3,
-        "D" : 1,
-        "@" : 3
+    outcomes = {
+        "AX" : 4,
+        "AY" : 8,
+        "AZ" : 3,
+
+        "BX" : 1,
+        "BY" : 5,
+        "BZ" : 9,
+        
+        "CX" : 7,
+        "CY" : 2,
+        "CZ" : 6
     }
 
     input = open(inputFile)
     score = 0
     
     for line in input:
-        line = line.split()
-        if line[1] == 'X':
-            score += points[chr(ord(line[0]) - 1)]
-        elif line[1] == 'Y':
-            score += 3 + points[line[0]]
-        elif line[1] == 'Z':
-            score += 6 + points[chr(ord(line[0]) + 1)]
+        score = score + outcomes["".join(line.strip().split())]
 
     return score
 
