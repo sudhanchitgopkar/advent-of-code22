79d3579442ada287ec6c830677152b20c93db87a
sudhanchitgopkar <sudhanchitgopkar@gmail.com>
2022-12-06 02:13:55 -0500

    🎉 Finished Day 6

---
+++ day6_2.py
@@ -0,0 +1,17 @@
+def solve(filename):
+    f = open(filename)
+
+    chars = [c for c in f.read().strip()]
+    seen = set()
+    l = 0
+
+    for r in range(len(chars)):
+        while chars[r] in seen or len(seen) > 14:
+            seen.remove(chars[l])
+            l += 1
+        seen.add(chars[r])
+        r += 1
+        if (len(seen) == 14):
+            return r
+
+print(solve("input/day6_1.txt"))
